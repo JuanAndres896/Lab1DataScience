f894eca3806196b423c0c3ebf6281b5bc62f927a
JuanAndres896 <juanandres896@icloud.com>
2018-08-09 22:53:13 -0600

    Initial commit

---
+++ Lab1.R
@@ -0,0 +1,9 @@
+# Universidad del Valle de Guatemala
+# Data Science 1 - Seccion 10
+# Juan Andres Garcia - 15046
+# Laboratorio No. 1 - Analisis exploratorio
+
+#library(readxl)
+
+casas = read.csv("train.csv", header = T) # Cargando el data set
+str(casas)
