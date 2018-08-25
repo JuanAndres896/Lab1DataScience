f0078565056a8892fbbaf2bae5d4ee45e0697f3d
JuanAndres896 <juanandres896@icloud.com>
2018-08-25 17:07:39 -0600

    Add files via upload

--- Lab1.R
+++ Lab1.R
@@ -1,9 +1,53 @@
-# Universidad del Valle de Guatemala
+# Univerisdad del Valle de Guatemala
 # Data Science 1 - Seccion 10
 # Juan Andres Garcia - 15046
-# Laboratorio No. 1 - Analisis exploratorio
+# Laboratorio 1  
 
-#library(readxl)
+#setwd("~/Sync/DATOS/DATA/LAB1")
 
-casas = read.csv("train.csv", header = T) # Cargando el data set
-str(casas)
+test<-read.csv("test.csv")
+train<-read.csv("train.csv")
+sample<-read.csv("sample_submission.csv")
+
+a<-vector()
+ntrain<-as.data.frame(train$Id)
+colnames(ntrain)<-colnames(train[1])
+for(i in 2:length(train)){
+  if(is.numeric(train[,i])){
+    a<-train[,i]
+    ntrain<-cbind(ntrain,a)
+    colnames(ntrain)[length(ntrain)]<-colnames(train[i])
+  }
+}
+
+
+a<-vector()
+ftrain<-as.data.frame(train$Id)
+colnames(ftrain)<-colnames(train[1])
+for(i in 2:length(train)){
+  if(is.factor(train[,i])){
+    a<-train[,i]
+    ftrain<-cbind(ftrain,a)
+    colnames(ftrain)[length(ftrain)]<-colnames(train[i])
+  }
+}
+
+
+cvars<-c("Discreta","Continua","Categórica","Discreta","Discreta","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Discreta","Discreta","Discreta","Discreta","Categórica","Categórica","Categórica","Categórica","Categórica","Discreta","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Discreta","Categórica","Discreta","Discreta","Discreta","Categórica","Categórica","Categórica","Discreta","Discreta","Discreta","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Categórica","Discreta","Categórica","Categórica","Discreta","Categórica","Discreta","Discreta","Categórica","Categórica","Categórica","Discreta","Discreta","Discreta","Discreta","Discreta","Discreta","Categórica","Categórica","Categórica","Discreta","Discreta","Discreta","Categórica","Categórica")
+vars<-cbind(vars,cvars)
+
+cors<-vector()
+b1<-vector()
+b2<-vector()
+b3<-vector()
+b4<-vector()
+for(i in 1:length(a)){
+  b1<-a[i]
+  b2[i]<-max(b1[b1<1])
+  b3[i]<-row.names(b1)[b1==b2[i]]
+  b4[i]<-colnames(a)[i]
+}
+cors<-as.data.frame(cbind(b4,b2,b3))
+colnames(cors)<-c("Variable","Correlacion","Variable Cor")
+
+scors<-a["SalePrice",c(-1,-38)]
